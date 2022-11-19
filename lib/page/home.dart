e9a4e5b3a141d7331d7e8f6e0caac37f504a50e5
dimaspsetyo <dimaspsetyo@gmail.com>
2022-11-19 11:17:15 +0700

    [Feature] Search user

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,4 +1,5 @@
 import 'package:apiaccurate/models/user_model.dart';
+import 'package:apiaccurate/widget/search_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:apiaccurate/logic/cubits/user_state.dart';
@@ -17,6 +18,14 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Accurate API"),
+        actions: [
+          IconButton(
+            onPressed: () {
+              showSearch(context: context, delegate: SearchUser());
+            },
+            icon: const Icon(Icons.search_sharp),
+          )
+        ],
       ),
       body: SafeArea(
         child: BlocConsumer<UserCubit, UserState>(
@@ -56,18 +65,56 @@ class _HomePageState extends State<HomePage> {
         itemBuilder: (context, index) {
           UserModel user = users[index];
 
-          return Padding(
+          return Card(
+              child: Padding(
             padding: const EdgeInsets.all(8),
-            child: Padding(
-                padding: const EdgeInsets.all(8),
-                child: Column(
-                  children: <Widget>[
-                    Text(user.name.toString(),
-                        style: const TextStyle(fontWeight: FontWeight.bold)),
-                    Text(user.city.toString()),
-                  ],
-                )),
-          );
+            child: ListTile(
+              title: Row(
+                children: [
+                  Container(
+                    width: 60,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      color: Colors.blueAccent,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Center(
+                      child: Text(
+                        user.id.toString(),
+                        style: const TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 20),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        user.name.toString(),
+                        style: const TextStyle(
+                          fontSize: 18,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        user.city.toString(),
+                        style: const TextStyle(
+                          fontSize: 14,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ));
         },
       );
     }
